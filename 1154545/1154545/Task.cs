12ec8468051f4f095de3fb580c7fea892c860215
angeleyy <lika.polyakova.2006@mail.ru>
2024-10-17 18:53:08 +0700

    Add files via upload

---
+++ 1154545/1154545/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1154545
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ 1154545/1154545/TaskController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using TaskManager.Models;
+
+
+namespace _1154545
+{
+    public class TaskController
+    {
+        private readonly List<Task> _tasks = new List<Task>();
+        public IActionResult Index()
+        {
+            return View(_tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Task task)
+        {
+            task.CreatedAt = DateTime.Now;
+            _tasks.Add(task);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var task = _tasks.Find(t => t.ID == id);
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            var existingTask = _tasks.Find(t => t.ID == task.ID);
+            existingTask.Title = task.Title;
+            existingTask.Description = task.Description;
+            existingTask.IsCompleted = task.IsCompleted;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var task = _tasks.Find(t => t.ID == id);
+            _tasks.Remove(task);
+            return RedirectToAction("Index");
+        }
+    }
+}
